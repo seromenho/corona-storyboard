21be89f0763b15291a342ffa4ab7b52846011790
Ricardo Seromenho <ricardo.seromenho@gmail.com>
2014-03-06 15:51:46 +0000

    First commit

---
+++ main.lua
@@ -0,0 +1,7 @@
+local storyboard = require("storyboard")
+
+-- set default screen background color to white
+display.setDefault( "background", 1, 1, 1 )
+
+-- Goto menu screen
+storyboard.gotoScene("menu")
